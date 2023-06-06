080fd517e5f7823cf5312c2c5bdbc811d8f3aaac
RoxaneDO <roxane.donnat@gmail.com>
2023-06-06 13:24:37 +0200

    Progressing on Project JS and Menu JS

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
                     scrollTextContent = container.find('.scrolling-text .scrolling-text-content').text(),
                     newScrollText = '';
                 for (var i = 0; i < windowToScrolltextRatio; i++) {
-                    newScrollText += ' ' + scrollTextContent;
+                    newScrollText += '' + scrollTextContent;
                 }
                 container.find('.scrolling-text .scrolling-text-content').text(newScrollText);
             }
@@ -165,125 +165,73 @@ $(document).ready(function() {
 //     })
 // });
 
-// PROGRESS BAR
-$(document).ready(function(e){
-        var progressing = 0;
-        var progressBar1 = document.querySelector('#projects_progressBar_1');
-        var progressBar2 = document.querySelector('#projects_progressBar_2');
-        var progressBar3 = document.querySelector('#projects_progressBar_3');
+// NAVBAR
+$(document).ready(function() {
+    const nav = document.querySelector('#nav_container');
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-        var distanceToTop = window.pageYOffset;
-        var keywords = document.querySelector('#keywords').getBoundingClientRect();
-        var projects = document.querySelector('#projects').getBoundingClientRect();
-        var projectsTitle = document.querySelector('#projects_title').getBoundingClientRect();
-        var projectsContainer = document.querySelector('#projects_container').getBoundingClientRect();
 
-        var progressBar1_bottom = progressBar1.getBoundingClientRect().bottom;
-        var progressBar2_bottom = progressBar2.getBoundingClientRect().bottom;
-        var progressBar3_bottom = progressBar3.getBoundingClientRect().bottom;
-
-        // STARTING PRONGRESSBAR IN END OF KEYWORD DIV
-        if(keywords.bottom > 0){
-            $('#projects_progressBar_1').css('height', 0 + '%');
+        if (scroll > 0){
+            nav.classList.remove("nav-container-mid")
+            nav.classList.add("nav-container-top")
         }
-        if(keywords.bottom <= 0){
-            progressing = Math.abs(keywords.bottom*0.25)
-            $('#projects_progressBar_1').css('height', progressing + '%');
-            if(progressing > 101) {
-                $('#projects_progressBar_1').css('height', 100 + '%');
-            }
+        if (scroll > 20){
+            nav.classList.remove("nav-container-bot")
+            nav.classList.remove("nav-container-top")
+            nav.classList.add("nav-container-mid")
         }
 
-        const project1 = document.querySelector('#project1')
-        const project2 = document.querySelector('#project2')
-        const project3 = document.querySelector('#project3')
-        const project4 = document.querySelector('#project4')
-        // const project5 = document.querySelector('#project5')
-        // const project6 = document.querySelector('#project6')
-
-        //var isOnProject1 = throwProject(distanceToTop, project1.getBoundingClientRect(), progressBar1_bottom, 0)
-        //console.log("isOnProject1", isOnProject1)
-        //
-        //
-        //var isComingOnProject2 = throwProject(distanceToTop, project2.getBoundingClientRect(), progressBar1_bottom, 10)
-        //progressingBar(isOnProject1, isComingOnProject2, progressBar2, 0.25);
-
-
-        var isOnProject2 = throwProject(distanceToTop, project2.getBoundingClientRect(), progressBar2_bottom, 0)
-        console.log("isOnProject2", isOnProject2)
-        // var isComingOnProject3 = throwProject(distanceToTop, project3.getBoundingClientRect(), progressBar2_bottom, 10)
-        // progressingBar(isOnProject2, isComingOnProject3, progressBar3, 0.25);
-
-
-        // var isOnProject3 = throwProject(distanceToTop, project3.getBoundingClientRect(), progressBar3_bottom, 10)
-
-
-
-        // var isOnProject4 = throwProject(distanceToTop, project4.getBoundingClientRect(), progressBar3_bottom, 10)
-        // var isOnProject5 = throwProject(distanceToTop, project5.getBoundingClientRect(), progressBar4_bottom, 10)
-        // var isOnProject6 = throwProject(distanceToTop, project6.getBoundingClientRect(), progressBar5_bottom, 10)
-
-
-
-        // progressingBar(isOnProject3, isOnProject4, progressBar3, 0.25);
-        // progressingBar(isOnProject3, isOnProject4, progressBar4, 0.25);
-        // progressingBar(isOnProject4, isOnProject5, progressBar5, 0.25);
-        // progressingBar(isOnProject5, isOnProject6, progressBar6, 0.25);
-
-
-        if(isOnProject1){
-            displayNone($(".project-detail", "#project1"), $(".project-detail", "#project2"), $(".project-detail", "#project2"))
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 20){
+            nav.classList.remove("nav-container-mid")
+            nav.classList.add("nav-container-bot")
         }
-        if(isComingOnProject2){
-            displayNone($(".project-detail", "#project2"), $(".project-detail", "#project1"), $(".project-detail", "#project3"))
+    })
+});
+
+
+// VIEWPORT
+var getElementsInArea = (function(docElm){
+    var viewportHeight = docElm.clientHeight;
+
+    return function(e, opts){
+        var found = [], i;
+
+        if( e && e.type == 'resize' )
+            viewportHeight = docElm.clientHeight;
+
+        for( i = opts.elements.length; i--; ){
+            viewportHeight = docElm.clientHeight;
+            var elm        = opts.elements[i],
+                pos        = elm.getBoundingClientRect(),
+                topPerc    = pos.top    / viewportHeight * 100,
+                bottomPerc = pos.bottom / viewportHeight * 100,
+                middle     = (topPerc + bottomPerc)/2,
+                inViewport = middle > opts.zone[0] &&
+                             middle < (100-opts.zone[1]);
+
+            elm.classList.toggle(opts.markedClass, inViewport);
+
+            var elmDetail   = elm.getElementsByClassName("project-detail")
+            elmDetail[0].classList.toggle("opa-100", inViewport);
+
+            if( inViewport )
+                found.push(elm);
         }
-        if(isComingOnProject3){
-            displayNone($(".project-detail", "#project3"), $(".project-detail", "#project2"), $(".project-detail", "#project4"))
-        }
-        // if(isOnProject4){
-        //     displayNone($(".project-detail", "#project4"), $(".project-detail", "#project3"), $(".project-detail", "#project5"))
-        // }
-        // if(isOnProject5){
-        //     displayNone($(".project-detail", "#project5"), $(".project-detail", "#project4"), $(".project-detail", "#project6"))
-        // }
-        // if(isOnProject6){
-        //     displayNone($(".project-detail", "#project6"), $(".project-detail", "#project5"), $(".project-detail", "#project5"))
-        // }
+    };
+})(document.documentElement);
+
+function f(e){
+    getElementsInArea(e, {
+        elements    : document.querySelectorAll('.project'),
+        markedClass : 'highlight--1',
+        zone        : [43, 43] // percentage distance from top & bottom
     });
+}
 
-    function displayNone(projectOpen, projectClose1, projectClose2){
-        projectClose1.addClass("d-none");
-        projectClose2.addClass("d-none");
-        projectOpen.removeClass("d-none");
-        projectOpen.addClass("d-block");
-    }
+window.addEventListener('scroll', f)
+window.addEventListener('resize', f)
 
-    function progressingBar(isBefore, isStart, progressBar, vitesse){
-        var progress;
-        if(isBefore){
-            var progress = 0
-            $(progressBar).css('height', 0 + '%');
-        }
-        if(isStart){
-            progressing++
-            $(progressBar).css('height', progressing*vitesse + '%');
-            if(progressing*vitesse > 101) {
-                $(progressBar).css('height', 100 + '%');
-            }
-        }
-    }
-
-    function throwProject(distanceToTop, project, progressBar, range){
-        console.log("top ", (Math.ceil(distanceToTop + project.top) - range) )
-        console.log("bottom ", (Math.ceil(distanceToTop + project.bottom) + range) )
-        console.log("progressBar ", Math.ceil(distanceToTop + progressBar) )
-        console.log("cond1", ((Math.ceil(distanceToTop + project.top) - range) <= Math.ceil(distanceToTop + progressBar)))
-        console.log("cond2", (Math.ceil(distanceToTop + project.bottom) + range > Math.ceil(distanceToTop + progressBar)))
-        return ((Math.ceil(distanceToTop + project.top) - range) <= Math.ceil(distanceToTop + progressBar)) && (((Math.ceil(distanceToTop + project.bottom) + range) >= Math.ceil(distanceToTop + progressBar)))
-    }
-})
 
 // PARALAX
 function parallaxe(element, vitesse, direction)
@@ -326,11 +274,11 @@ function parallaxe(element, vitesse, direction)
     }
 
 }
-parallaxe('#triangle', 0.4, 'top');
-parallaxe('#triangle', 0.2, 'right');
-parallaxe('#header-square', 0.8, 'top');
+parallaxe('#whoiam_bg', 0.4, 'top');
+parallaxe('#whoiam_bg', 0.2, 'right');
+parallaxe('#header_square', 0.8, 'top');
 
-// SCROLL SQUARE
+// SCROLL SQUARE SKILLS
 function paralaxeScale(element, vitesse){
     var transform = $(element).css('width').split('px');
     var width = parseFloat(transform[0]);
